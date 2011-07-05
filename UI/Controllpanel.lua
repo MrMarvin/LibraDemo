2645d9309d8917b34245fbaed6a2fc4960bbdede
Marv Cool <marv@hostin.is>
2011-07-05 17:35:11 +0200

    some changes

--- UI/Controllpanel.lua
+++ UI/Controllpanel.lua
@@ -2,7 +2,7 @@ print("Welcome to LibraDemo. Controllpanel loading...")
 local params = {["titletext"]="Controllpanel", ["size"]={["height"]=100, ["width"]=100}}
 local window = Libra.UI.Window:Create(params)
 --window:Resize(200,500)
-window:Show()
+window:hide()
 
 local context = UI.CreateContext("foo")
 print("ContextWidth:" .. UIParent:GetWidth())
--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
 Libra = LibStub:GetLibrary("Libra-alpha")
+
+LibraDemo = Libra.Addon:Create("LibraDemo")
+
+local function generalSlashCommandTaker(commandline)
+  print("Commandline caught: "..commandline)
+  
+  if commandline:match(".*show.*") then
+    
+  end
+end
+
+LibraDemo:RegisterSlashCommand("LibraDemo","LibraDemo",generalSlashCommandTaker,"LibraDemo description text" )
